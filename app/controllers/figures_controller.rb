7d284ee350e541b3b6a988ad77b4442d20301ef8
Suchitra Naik <susanaik@gmail.com>
2018-06-27 18:55:07 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -37,28 +37,19 @@ class FiguresController < ApplicationController
 
 
   post '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id])
-
-     @figure.name = params["figure_name"]
-
-     if params["new_title"] != ""
-       @figure.titles << Title.find_or_create_by(name: params["new_title"])
-     end
-
-     if params[:landmark] != nil
-      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
-    else
-      if params["new_landmark"] != ""
+    @figure = Figure.find(params[:id])
+      @figure.update(params[:figure])
+      
+      if !params[:landmark][:name].empty?
         @figure.landmarks << Landmark.create(params[:landmark])
       end
-    end
 
-     if params[:figure][:landmark_ids] != nil
-       @figure.landmarks << Landmark.find_by_id(params[:figure][:landmark_ids])
-     end
-
-     @figure.save
-     redirect "/figures/#{@figure.id}"
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+      
+      @figure.save
+      redirect to "/figures/#{@figure.id}"
   end
 
 end
