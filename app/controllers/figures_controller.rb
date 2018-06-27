023b5f0684bda3de41dc4b3b943306fb213470f7
Suchitra Naik <susanaik@gmail.com>
2018-06-27 19:00:39 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -38,7 +38,7 @@ class FiguresController < ApplicationController
 
   post '/figures/:id' do
     @figure = Figure.find(params[:id])
-      @figure = params[:figure]
+      @figure.name = params[:figure]
 
       if !params[:landmark][:name].empty?
         @figure.landmarks << Landmark.create(params[:landmark])
